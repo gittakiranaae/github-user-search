a6ab44dc67d910e10365ba4bcc4ad67eb7dd7977
gittakiranaae <gittakiranaae@gmail.com>
2023-01-25 17:15:19 +0100

    add bio header

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,8 +6,17 @@ function Profile() {
   return (
     <>
       <div className="profile-container">
-        <div className="image-cropper">
-          <img id="profile-picture" src="src\images\2pi_square.jpeg"></img>
+        <div className="bio-header">
+          <div className="column">
+            <div className="image-cropper">
+              <img id="profile-picture" src="src\images\2pi_square.jpeg"></img>
+            </div>
+          </div>
+          <div className="column">
+            <p className="name">Gitta</p>
+            <p className="username">@gittagittu</p>
+            <p className="joined-since">Joined 25 Jan 2023</p>
+          </div>
         </div>
       </div>
     </>
