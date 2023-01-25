f7caf26a14cfb8f0dd3434af3f3343131173bc4f
gittakiranaae <gittakiranaae@gmail.com>
2023-01-25 18:36:34 +0100

    add repos followers following

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 
 function Profile() {
   // const [count, setCount] = useState(0);
+  const numbers = { Repos: 8, Followers: 3938, Following: 9 };
   return (
     <>
       <div className="profile-container">
@@ -17,7 +18,19 @@ function Profile() {
             <p className="username">@gittagittu</p>
             <p className="joined-since">Joined 25 Jan 2023</p>
           </div>
+          <div></div>
         </div>
+        <div className="bio-desc"></div>
+        <div className="bio-numbers">
+          {Object.keys(numbers).map((number) => (
+            <div className="number">
+              {number}
+              <br />
+              {numbers[number]}
+            </div>
+          ))}
+        </div>
+        <div className="more-info"></div>
       </div>
     </>
   );
